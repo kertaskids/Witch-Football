110f68ac98416325b059692ac5b7109a6cba3f91
kertaskids <kertaskids@gmail.com>
2019-07-08 15:11:20 +0700

    Add BGM-Spring, Revise Textures

---
+++ Witch Football/Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    public float initialRotationX = -45f;
+    public float targetRotationX = 35f;
+    
+    private Quaternion targetRot;
+
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(initialRotationX, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        targetRot = Quaternion.Euler(targetRotationX, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);    
+    }
+
+    void Update()
+    {   
+        transform.rotation =  Quaternion.Slerp(transform.rotation, targetRot, 0.5f *  Time.deltaTime);
+        if(Quaternion.Angle(transform.rotation, targetRot) == 0){
+            transform.rotation = Quaternion.Euler(targetRotationX, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+            Destroy(this);
+        }
+    }
+}
---
+++ Witch Football/Assets/Scripts/RandomBGM.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBGM : MonoBehaviour
+{
+    public AudioClip[] soundtracks;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if(!audioSource.playOnAwake){
+            RandomPlay();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate() {
+        if(!audioSource.isPlaying){
+            RandomPlay();
+        }
+    }
+
+    void RandomPlay(){
+        audioSource.clip = soundtracks[Random.Range(0, soundtracks.Length)];
+        audioSource.Play();
+    }
+}
